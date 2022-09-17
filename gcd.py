1db767037febdaac5330059781f1bf8fd2d9316c
Rab-son <rabsonsayendajnr@gmail.com>
2022-09-17 14:49:15 +0200

    completed code logic

--- gcd.py
+++ gcd.py
@@ -1,3 +1,14 @@
 #1. For two integers a and b, where a > b, divide a by b
 #2. If the remainder, r, is 0, then stop: GCD is b.
 #3. Otherwise, set a to b, b to r, and repeat at step 1 until r is 0
+
+def gcd(a, b):
+    while (b != 0):
+        t = a
+        a = b
+        b = t % b
+
+    return a
+
+print(gcd(100, 84))
+print(gcd(20, 81))
